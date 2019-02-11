79b9e55b6f677cdeba0fecc8d1fa781a707e7731
ZhenyeZhang <2411335z@student.gla.ac.uk>
2019-02-11 00:04:53 +0000

    Chapter 3 second fixed problems

--- rango/views.py
+++ rango/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Rango says hey there partner! <vr/> <a href='/rango/about/'>About</a>")
 
-def about(request):<a href="/rango/">Index</a>
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>View index page</a>")
+def about(request):
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
