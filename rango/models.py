8ef3075d00049d2c013c077230a40003b1902d32
ZhenyeZhang <2411335z@student.gla.ac.uk>
2019-02-12 11:56:08 +0000

    chapter 9

--- rango/models.py
+++ rango/models.py
@@ -1,21 +1,29 @@
-from __future__ import unicode_literals
-
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    website = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(blank=True, unique=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
 
     class Meta:
-        verbose_name_plural = 'Category'
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
@@ -26,9 +34,6 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
-
-    class Meta:
-        verbose_name_plural = 'Page'
 
     def __str__(self):
         return self.title
