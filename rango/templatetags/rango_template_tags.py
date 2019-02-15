538dc70c884ae8d7e2e64c82c9aed470695207a2
ZhenyeZhang <2411335z@student.gla.ac.uk>
2019-02-15 17:11:02 +0000

    chapter 10end

--- rango/templatetags/rango_template_tags.py
+++ rango/templatetags/rango_template_tags.py
@@ -3,7 +3,6 @@ from rango.models import Category
 
 register = template.Library()
 
-
 @register.inclusion_tag('rango/cats.html')
 def get_category_list(cat=None):
     return {'cats': Category.objects.all(),
