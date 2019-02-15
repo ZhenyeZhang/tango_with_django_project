17a36a48a34b4f6ceaeafe5a60686dabf54fc181
ZhenyeZhang <2411335z@student.gla.ac.uk>
2019-02-15 17:30:16 +0000

    chapter5end

--- populate_rango.py
+++ populate_rango.py
@@ -1,5 +1,4 @@
 import os
-import random
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'tango_with_django_project.settings')
 
@@ -52,14 +51,14 @@ def populate():
             print("- {0} - {1}".format(str(c), str(p)))
 
 
-def add_page(cat, title, url):
+def add_page(cat, title, url, views=0):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
-    p.views=random.randint(0, 33)
+    p.views=views
     p.save()
     return p
 
-def add_cat(name, views, likes):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
     c.views=views
     c.likes=likes
